adba82b0f3e3700a767b34c374328b9626167741
gayol-openrxiv <226899647+gayol-openrxiv@users.noreply.github.com>
2026-09-14 14:54:59 -0500

    Add test for capitalization with name

--- tests/testthat/test-compliment.R
+++ tests/testthat/test-compliment.R
@@ -14,6 +14,17 @@ test_that("compliment() errors on invalid name", {
   expect_error(compliment(c("A", "B")))
 })
 
+test_that("compliment() preserves capital I when name is supplied", {
+  set.seed(8391)
+  results <- replicate(200, compliment("Alex"))
+  i_results <- results[grepl("I really admire", results)]
+  expect_true(
+    length(i_results) > 0,
+    label = "No 'I really admire' templates appeared; increase replications"
+  )
+  expect_true(all(grepl("^Alex, I really admire", i_results)))
+})
+
 test_that("compliment() produces varied output", {
   set.seed(42)
   results <- replicate(50, compliment())
